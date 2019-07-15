01bc1d732b333cf9dece5a5e2b1894b367fc0f92
Mika Sormunen <mika.sormunen@citrus.fi>
2019-07-16 01:20:07 +0300

    Prepare deployment

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -4,6 +4,17 @@ import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -17,7 +28,7 @@ const GithubState = props => {
   // Search users
   const searchUsers = async searchText => {
     setLoading();
-    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     const res = await axios.get(`https://api.github.com/search/users?q=${searchText}`);
     //setUsers(res.data.items);
 
@@ -27,18 +38,24 @@ const GithubState = props => {
   // Get user by login
   const getUser = async username => {
     setLoading();
-    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     const res = await axios.get(`https://api.github.com/users/${username}`);
 
-    dispatch({ type: GET_USER, payload: res.data });
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
   };
 
   // Get user's repos
   const getUserRepos = async username => {
     setLoading();
-    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    //const res = await axios.get('https://api.github.com/search/users?q={searchText}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:desc`);
-    dispatch({ type: GET_REPOS, payload: res.data });
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
   };
 
   // Clear users
